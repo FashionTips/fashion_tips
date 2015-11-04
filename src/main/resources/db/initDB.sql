11ec02c179d044f3e9027b56458c1c3adf770950
Elginior <elginior@gmail.com>
2015-11-04 12:40:57 +0200

    Create init settings

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS requests;
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START 100000;
+
+CREATE TABLE users
+(
+  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name       VARCHAR,
+  email      VARCHAR NOT NULL,
+  password   VARCHAR NOT NULL,
+  registered TIMESTAMP DEFAULT now()
+);
+CREATE UNIQUE INDEX unique_email ON USERS (email);
+
+CREATE TABLE requests
+(
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id     INTEGER NOT NULL,
+  name        VARCHAR NOT NULL,
+  description TEXT,
+  datetime    TIMESTAMP NOT NULL DEFAULT now(),
+  pic_url     VARCHAR,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE comments
+(
+  id            INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id       INTEGER NOT NULL,
+  request_id    INTEGER NOT NULL,
+  comment_text  TEXT,
+  datetime      TIMESTAMP NOT NULL DEFAULT now(),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (request_id) REFERENCES requests (id) ON DELETE CASCADE
+);
