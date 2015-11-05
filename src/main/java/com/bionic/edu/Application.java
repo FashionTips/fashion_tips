0facffe6118dee41156f94f9553608571a0a8492
Maxim Solomkin <solomkinmv@gmail.com>
2015-11-05 23:06:08 +0200

    Updated application entry point to use Spring.

--- src/main/java/com/bionic/edu/Application.java
+++ src/main/java/com/bionic/edu/Application.java
@@ -1,18 +1,24 @@
 package com.bionic.edu;
 
+import com.bionic.edu.crud.UserDao;
 import com.bionic.edu.crud.UserService;
 import com.bionic.edu.entity.Usr;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by maxim on 11/5/15.
  */
 public class Application {
     public static void main(String[] args) {
-        UserService us = new UserService();
-//        Usr usr = new Usr("Max", "solomkinmv", "solomkinmv@gmail.com", "1111", "");
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        UserService userService = context.getBean(UserService.class);
+        Usr usr = new Usr("Max", "solomkinmv", "solomkinmv@gmail.com", "1111", "");
 //        System.out.println(usr);
-//        us.add(usr);
+//        userService.create(usr);
+//        us.create(usr);
 //        System.out.println(usr);
-        System.out.println(us.get(3));
+        System.out.println(userService.get(1));
+//        System.out.println(userService.read(3));
     }
 }
