a0ecde1cc2399006615f48d52e54cd7af336e0bd
Maxim Solomkin <solomkinmv@gmail.com>
2015-11-18 21:22:54 +0200

    Updated configuration.

---
+++ persistence/main/resources/db/filloutDB.sql
@@ -0,0 +1,5 @@
+DELETE FROM users;
+
+INSERT INTO users (login, email, password) VALUES ('login1', 'email1@example.com', '1111');
+INSERT INTO users (login, email, password) VALUES ('login2', 'email2@example.com', '1111');
+INSERT INTO users (login, email, password) VALUES ('login3', 'email3@example.com', '1111');
