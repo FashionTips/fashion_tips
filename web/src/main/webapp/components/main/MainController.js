3dafc2b4fceb295daeda1f2833a9fd0cec827704
slav9nin <slav9nin2009@gmail.com>
2015-11-21 16:38:08 +0200

    Integrated with web module.

---
+++ web/src/main/webapp/components/main/MainController.js
@@ -0,0 +1,47 @@
+var MainController = function ($rootScope, $scope, $http, $location) {
+
+    var authenticate = function (credentials, callback) {
+
+        var headers = credentials ? {
+            authorization: "Basic "
+            + btoa(credentials.username + ":" + credentials.password)
+        } : {};
+
+        $http.get("http://localhost:8080/users/1", {headers: headers}).success(function (data) {
+            if (data.login) {
+                $rootScope.authenticated = true;
+            } else {
+                $rootScope.authenticated = false;
+            }
+            callback && callback();
+        }).error(function () {
+            $rootScope.authenticated = false;
+            callback && callback();
+        });
+
+    }
+
+    authenticate();
+    $scope.credentials = {};
+    $scope.login = function () {
+        authenticate($scope.credentials, function () {
+            if ($rootScope.authenticated) {
+                $location.path("/profile");
+                $scope.error = false;
+            } else {
+                $location.path("/");
+                $scope.error = true;
+            }
+        });
+    };
+
+    $scope.logout = function () {
+        $rootScope.authenticated = false;
+        $location.path("/");
+    }
+
+    $scope.clickMe = function () {
+        $http.get("http://localhost:8080/users");
+    }
+
+};
