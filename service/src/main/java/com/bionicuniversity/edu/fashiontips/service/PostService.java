691898b10cf51cd6ba84a7f08f577c79b8f46fae
barik111 <core.safecrlf>
2015-11-25 23:56:12 +0200

    add PostService+PostServicImpl for CRUD operations

---
+++ service/src/main/java/com/bionicuniversity/edu/fashiontips/service/PostService.java
@@ -0,0 +1,11 @@
+package com.bionicuniversity.edu.fashiontips.service;
+
+import com.bionicuniversity.edu.fashiontips.entity.Post;
+
+/**
+ * Created by Sergiy on 25.11.2015
+ * Project: fashion-tips
+ * Post Service Interface.
+ */
+public interface PostService extends GenericService<Post, Long>{
+}
---
+++ service/src/main/java/com/bionicuniversity/edu/fashiontips/service/impl/PostServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bionicuniversity.edu.fashiontips.service.impl;
+
+import com.bionicuniversity.edu.fashiontips.entity.Post;
+import com.bionicuniversity.edu.fashiontips.service.PostService;
+
+import javax.inject.Named;
+
+/**
+ * Created by Sergiy on 25.11.2015
+ * Project: fashion-tips
+ * Post Service Implementation.
+ */
+@Named
+public class PostServiceImpl extends GenericServiceImpl<Post, Long> implements PostService {
+}
