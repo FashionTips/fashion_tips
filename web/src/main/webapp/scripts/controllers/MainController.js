5f850cf61f1ba41fedd87d809925c25d58310d9e
Elginior <elginior@gmail.com>
2016-01-18 16:04:22 +0200

    Add searching by category on frontend FT12-101

--- web/src/main/webapp/scripts/controllers/MainController.js
+++ web/src/main/webapp/scripts/controllers/MainController.js
@@ -3,10 +3,13 @@ var MainController = ['$scope', 'postService', function ($scope, postService) {
     var qUrl = /q=([^&]+)/.exec(document.location.search);
     var q = qUrl === null ? undefined : qUrl[1].replace('%23', '#');      // get value of q url-parameter
 
+    var categoryUrl = /category=([^&]+)/.exec(document.location.search);
+    var category = categoryUrl === null ? undefined : categoryUrl[1];      // get value of category url-parameter
+
     $scope.posts = [];
 
     /* requests all posts and assigns them to scope variable */
-    postService.getAll(q)
+    postService.getAll(q, null, category)
         .then(function (data) {
             $scope.posts = data;
         });
